5f8ed48e6030207ae549e11178ff9611737e13e7
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2019-08-05 12:37:43 -0400

    quatrotate now accepts quaternitons and vectors as either column or row vectors

--- quatrotate.m
+++ quatrotate.m
@@ -10,10 +10,26 @@
 % this is not part of the Dartmouth site license
 function v_out = quatrotate(q_in,v_in)
 
+% make sure q_in and v_in are the correct length and shape
+if (numel(q_in) == 4)
+    q_in = reshape(q_in,4,1);
+else
+    error('Quaternion must have 4 elements.');
+end
+if (numel(v_in) == 3)
+    v_in_size = size(v_in);
+    v_in = reshape(v_in,3,1);
+else
+    error('Vector to rotate must have 3 elements.');
+end
+
 % extract scalar and vector parts of quaternion
 q0   = q_in(1);   % real (scalar) part of quaternion
 q123 = q_in(2:4); % imaginary (vector) part of quaternion
 v_out = (q0^2-norm(q123)^2)*v_in + 2*dot(q123,v_in)*q123 + 2*q0*cross(q123,v_in);
+
+% reshape output vector according to format of input vector (col vs. row)
+v_out = reshape(v_out,v_in_size);
 
 % alternatively, an equivalent (albeit more computationally expensive) method
 % is to rotate v_in using point rotation directly: v_out = q * v_in * conj(q)
