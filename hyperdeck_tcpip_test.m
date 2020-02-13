31dddb91a57f59c0ea4889091324a03f0221b84a
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-02-13 14:28:57 -0500

    started a hyperdeck tcpip test script

---
+++ hyperdeck_tcpip_test.m
@@ -0,0 +1,63 @@
+%% restart
+close all; clear; clc;
+
+% close all files and connections
+fclose('all');
+
+% create TCPIP objects for each Hyper Deck
+% TODO: this shouldn't be hard coded!!
+hyperDeckLeft = tcpip('192.168.10.50',9993,'Timeout',0.1);
+hyperDeckRight = tcpip('192.168.10.60',9993,'Timeout',0.1);
+
+% open both hyperdeck TCPIP channels
+fopen(hyperDeckLeft);
+fopen(hyperDeckRight);
+if(~strcmp(hyperDeckLeft.status,'open'))
+    error('Error opening LEFT HyperDeck TCP/IP channel.');
+end
+if(~strcmp(hyperDeckRight.status,'open'))
+    error('Error opening RIGHT HyperDeck TCP/IP channel.');
+end
+%%
+% flush input and output buffers
+flushinput(hyperDeckLeft);
+flushoutput(hyperDeckLeft);
+flushinput(hyperDeckRight);
+flushoutput(hyperDeckRight);
+
+% start recording on both Hyper Decks
+% Note: \n seems to work in MATLAB on windows, need \r\n in python
+fprintf(hyperDeckLeft,'ping\n');
+fprintf(hyperDeckRight,'ping\n');
+respL = fgets(hyperDeckLeft);
+respR = fgets(hyperDeckRight);
+errorVec = zeros(1,2);
+if(isempty(respL) || ~strcmp(respL(1:6),'200 ok'))
+    errorVec(1) = 1;
+end
+if(isempty(respR) || ~strcmp(respR(1:6),'200 ok'))
+    errorVec(2) = 1;
+end
+if(prod(errorVec))
+    error('Comms issue!');
+end
+fprintf(hyperDeckLeft,'remote: enable: true\n');
+fprintf(hyperDeckRight,'remote: enable: true\n');
+respL = fgets(hyperDeckLeft);
+respR = fgets(hyperDeckRight);
+char(respL)
+char(respR)
+
+fprintf(hyperDeckLeft,'slot select: slot id: 1\n');
+fprintf(hyperDeckRight,'slot select: slot id: 1\n');
+respL = fgets(hyperDeckLeft);
+respR = fgets(hyperDeckRight);
+char(respL)
+char(respR)
+
+
+fprintf(hyperDeckLeft,'record\n');
+fprintf(hyperDeckRight,'record\n');
+%%
+fclose(hyperDeckLeft);  
+fclose(hyperDeckRight);
