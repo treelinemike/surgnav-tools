7d3ae6efece2a6ce5a7c69d1ba791f4ed34a68c1
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-03-02 11:00:52 -0500

    updated hyperdeck testing with system ping, etc.

--- hyperdeck_tcpip_test.m
+++ hyperdeck_tcpip_test.m
@@ -6,8 +6,24 @@ fclose('all');
 
 % create TCPIP objects for each Hyper Deck
 % TODO: this shouldn't be hard coded!!
-hyperDeckLeft = tcpip('192.168.10.50',9993,'Timeout',0.1);
-hyperDeckRight = tcpip('192.168.10.60',9993,'Timeout',0.1);
+hyperDeckLeftIP = '192.168.10.50';
+hyperDeckRightIP = '192.168.10.60';
+
+% first see if ports alive
+if(isAlive(hyperDeckLeftIP) == 1)
+    disp('Left Hyperdeck Ready');
+else
+    warning('Left Hyperdeck NOT Ready!');
+end
+if(isAlive(hyperDeckRightIP) == 1)
+    disp('Right Hyperdeck Ready');
+else
+    warning('Right Hyperdeck NOT Ready!');
+end
+
+% create serial objects
+hyperDeckLeft = tcpip(hyperDeckLeftIP,9993,'Timeout',0.1);
+hyperDeckRight = tcpip(hyperDeckRightIP,9993,'Timeout',0.1);
 
 % open both hyperdeck TCPIP channels
 fopen(hyperDeckLeft);
@@ -41,23 +57,41 @@ end
 if(prod(errorVec))
     error('Comms issue!');
 end
+
 fprintf(hyperDeckLeft,'remote: enable: true\n');
 fprintf(hyperDeckRight,'remote: enable: true\n');
 respL = fgets(hyperDeckLeft);
 respR = fgets(hyperDeckRight);
-char(respL)
-char(respR)
+strtrim(char(respL))
+strtrim(char(respR))
 
 fprintf(hyperDeckLeft,'slot select: slot id: 1\n');
 fprintf(hyperDeckRight,'slot select: slot id: 1\n');
 respL = fgets(hyperDeckLeft);
 respR = fgets(hyperDeckRight);
-char(respL)
-char(respR)
+strtrim(char(respL))
+strtrim(char(respR))
 
 
 fprintf(hyperDeckLeft,'record\n');
 fprintf(hyperDeckRight,'record\n');
+respL = fgets(hyperDeckLeft);
+respR = fgets(hyperDeckRight);
+strtrim(char(respL))
+strtrim(char(respR))
 %%
 fclose(hyperDeckLeft);  
 fclose(hyperDeckRight);
+
+
+function status = isAlive(ipAddr,timeout)
+    [sysStatus,sysOut] = system(['ping -n 1 -w 10 ' ipAddr ' | grep ''% loss'' | sed ''s/.*(\(.*\)\% loss),/\1/''']);
+    if(str2num(sysOut) == 0)
+        status = 1;
+    else
+        status = 0;
+    end
+ end
+
+
+
