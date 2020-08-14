e73a14e53cfbd7763ebfd3fe8022c0a00806c12b
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-08-14 11:32:17 -0400

    updated dicom_explore_a.m to do correct HU -> gray conversion, etc.

--- dicom_explore_a.m
+++ dicom_explore_a.m
@@ -1,11 +1,24 @@
+% restart
 close; clear; clc; 
-filename = 'E:\901561\901563\901752';
-im = dicomread(filename);
-im1 = im-min(im(:));
-im2 = double(im1)/double(max(im1(:)));
-imshow(im2);
 
-a = dicominfo(filename);
-seriesNo = a.SeriesNumber
-seriesDate = a.SeriesDate
-acqTime = a.AcquisitionTime
+% options
+filename = 'H:\CT\31584-010\CT 9001-9004\900403';
+minHUScaleVal = -208;
+maxHUScaleVal = 218;
+
+% load DICOM info
+dinf = dicominfo(filename);
+
+% load DICOM image and adjust grey values
+im = dicomread(filename);
+im = double(im)*dinf.RescaleSlope + dinf.RescaleIntercept;  % will need coversion to uint8 to be standardized!
+im = uint8((im-minHUScaleVal)*(255/(maxHUScaleVal-minHUScaleVal))); % rescale HU to grayscale based on a "pretty good" mapping identified in Mimics
+
+% show image
+imshow(im);
+
+% extract and show relevant metadata
+seriesNo = dinf.SeriesNumber
+seriesDate = dinf.SeriesDate
+acqTime = dinf.AcquisitionTime
+imPosPt = dinf.ImagePositionPatient(3)
