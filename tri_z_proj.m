1d9bb3afa9108933427832b406c7916f488b9b79
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-12-18 16:27:20 -0500

    tweaked to make a function, about to delete all plotting

--- tri_z_proj.m
+++ tri_z_proj.m
@@ -1,31 +1,29 @@
-%%
-% function proj_poly = tri_z_proj(tri)
-close all; clear; clc;
-tri = stlread('C:\Users\f002r5k\GitHub\matlab-examples\candycane.stl');
-v_tris = vertexAttachments(tri);
 
-rng(4133);  % 1231
-R = tang2matrix(randRotUnifAng_t([0,0,0]',pi,1))
-
+function proj_poly = tri_z_proj(tri,h)
+% close all; clear; clc;
+% tri = stlread('C:\Users\f002r5k\GitHub\matlab-examples\candycane.stl');
+% h = 1;
+%
+% % rng(4233);  % 1231
+% % R = tang2matrix(randRotUnifAng_t([0,0,0]',pi,1))
+% 
 % 
 % R = [    0.1543         0   -0.9880
 %          0    1.0000         0
 %     0.9880         0    0.1543];
 
 % merge duplicate vertices
-proj_pts = tri.Points*R';
-proj_pts = proj_pts(:,1:2);
-% proj_pts = tri.Points(:,1:2);
+% proj_pts = tri.Points*R';
+% proj_pts = proj_pts(:,1:2);
+proj_pts = tri.Points(:,1:2);
 proj_pts_unique = unique(proj_pts,'rows');
 [~,vertex_map] = ismember(proj_pts,proj_pts_unique,'rows');
 proj_cl = [vertex_map(tri.ConnectivityList(:,1)) vertex_map(tri.ConnectivityList(:,2)) vertex_map(tri.ConnectivityList(:,3))];
-proj_tri = triangulation(proj_cl,proj_pts_unique);
 
 % sample space
 % N_pts = 100; 
 % samp_x_vals = linspace(min(proj_pts_unique(:,1)),max(proj_pts_unique(:,1)),N_pts)
 % samp_y_vals = linspace(min(proj_pts_unique(:,2)),max(proj_pts_unique(:,2)),N_pts)
-h = 1; %[mm]
 samp_x_vals = min(proj_pts_unique(:,1)):h:max(proj_pts_unique(:,1));
 samp_y_vals = min(proj_pts_unique(:,2)):h:max(proj_pts_unique(:,2));
 [XX,YY] = meshgrid(samp_x_vals,samp_y_vals);
@@ -61,32 +59,37 @@ for tri_idx = 1:size(proj_cl,1)
    end 
    qp_bkgnd_mask = qp_bkgnd_mask & (~qp_mask_local);
 end
-%%
-figure;
-ax = subplot(1,3,1);
-hold on; grid on; axis equal;
-pp = patch('Faces',proj_tri.ConnectivityList,'Vertices',proj_tri.Points,'FaceColor',[0.3 0.3 0.3],'EdgeColor',[0.3 0.3 0.3],'LineWidth',0.5);
-% patch('Faces',proj_tri.ConnectivityList,'Vertices',proj_tri.Points,'FaceColor','none','EdgeColor',[0 0 0],'LineWidth',0.5);
-plot(qp(:,1),qp(:,2),'r.','MarkerSize',4);
-plot(qp(~qp_bkgnd_mask,1),qp(~qp_bkgnd_mask,2),'b.','MarkerSize',4);
 
+% compute boudary
 qp_interior = qp(~qp_bkgnd_mask,:);
 qp_bnd_idx = boundary(qp_interior,1);
 qp_bnd = qp_interior(qp_bnd_idx,:);
 
-ax(end+1) = subplot(1,3,2);
-hold on; grid on; axis equal;
-plot(qp_bnd(:,1),qp_bnd(:,2),'r-','LineWidth',2);
-
 % construct shadow polygon
 proj_poly = polyshape( qp_bnd(:,1), qp_bnd(:,2),'Simplify',false);
-poly_area = area(proj_poly);
-[poly_cx poly_cy] = centroid(proj_poly);
-fprintf('Polygon area: %6.3f\n',poly_area);
-fprintf('Polygon centroid: (%6.3f,%6.3f)\n',poly_cx,poly_cy);
 
-ax(end+1) = subplot(1,3,3);
-hold on; grid on; axis equal;
-plot(proj_poly)
-plot(poly_cx,poly_cy,'r.','MarkerSize',20);
-linkaxes(ax,'xy');
+end
+
+% % display stuff
+% proj_tri = triangulation(proj_cl,proj_pts_unique);
+% figure;
+% ax = subplot(1,3,1);
+% hold on; grid on; axis equal;
+% pp = patch('Faces',proj_tri.ConnectivityList,'Vertices',proj_tri.Points,'FaceColor',[0.3 0.3 0.3],'EdgeColor',[0.3 0.3 0.3],'LineWidth',0.5);
+% plot(qp(:,1),qp(:,2),'r.','MarkerSize',4);
+% plot(qp(~qp_bkgnd_mask,1),qp(~qp_bkgnd_mask,2),'b.','MarkerSize',4);
+% 
+% ax(end+1) = subplot(1,3,2);
+% hold on; grid on; axis equal;
+% plot(qp_bnd(:,1),qp_bnd(:,2),'r-','LineWidth',2);
+% 
+% poly_area = area(proj_poly);
+% [poly_cx poly_cy] = centroid(proj_poly);
+% fprintf('Polygon area: %6.3f\n',poly_area);
+% fprintf('Polygon centroid: (%6.3f,%6.3f)\n',poly_cx,poly_cy);
+% 
+% ax(end+1) = subplot(1,3,3);
+% hold on; grid on; axis equal;
+% plot(proj_poly)
+% plot(poly_cx,poly_cy,'r.','MarkerSize',20);
+% linkaxes(ax,'xy');
