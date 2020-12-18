89ee5b820705ef76f8c968cd8b24ebf14169fcf9
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2020-12-18 14:07:54 -0500

    ugh. doesn't work... fails if intersections happend at non-nodes.  try again with sampling gridpoints

--- tri_z_proj.m
+++ tri_z_proj.m
@@ -3,22 +3,34 @@ close all; clear; clc;
 tri = stlread('C:\Users\f002r5k\GitHub\matlab-examples\candycane.stl');
 v_tris = vertexAttachments(tri);
 
-rng(1233);  % 1231
-R = tang2matrix(randRotUnifAng_t([0,0,0]',pi,1))
+% rng(3127);  % 1231
+% R = tang2matrix(randRotUnifAng_t([0,0,0]',pi,1))
+
+
+R = [    0.1543         0   -0.9880
+         0    1.0000         0
+    0.9880         0    0.1543];
 
 % merge duplicate vertices
 proj_pts = tri.Points*R';
 proj_pts = proj_pts(:,1:2);
+% proj_pts = tri.Points(:,1:2);
 proj_pts_unique = unique(proj_pts,'rows');
-[~,vertex_map] = ismember(proj_pts,proj_pts_unique,'rows'); 
+[~,vertex_map] = ismember(proj_pts,proj_pts_unique,'rows');
 proj_cl = [vertex_map(tri.ConnectivityList(:,1)) vertex_map(tri.ConnectivityList(:,2)) vertex_map(tri.ConnectivityList(:,3))];
 proj_tri = triangulation(proj_cl,proj_pts_unique);
 
 figure;
 hold on; grid on; axis equal;
-patch('Faces',proj_tri.ConnectivityList,'Vertices',proj_tri.Points,'FaceColor',[0.3 0.3 0.3],'EdgeColor',[0.3 0.3 0.3],'LineWidth',0.5);
+pp = patch('Faces',proj_tri.ConnectivityList,'Vertices',proj_tri.Points,'FaceColor',[0.3 0.3 0.3],'EdgeColor',[0.3 0.3 0.3],'LineWidth',0.5);
 patch('Faces',proj_tri.ConnectivityList,'Vertices',proj_tri.Points,'FaceColor','none','EdgeColor',[0 0 0],'LineWidth',0.5);
 plot( proj_tri.Points(:,1), proj_tri.Points(:,2), 'r.', 'MarkerSize', 10 );
+
+% try this with MESHGRID instead.... 
+
+% nfv = reducepatch(pp)
+% patch('Faces',nfv.faces,'Vertices',nfv.vertices(:,1:2),'FaceColor',[1 1 1],'EdgeColor',[0 0 0],'LineWidth',0.5);
+
 
 % starting point, closest to lower right corner
 pts = proj_pts_unique;
@@ -39,7 +51,7 @@ for i = 1:length(neighbor_nodes)
     for j = (i+1):length(neighbor_nodes)
         v1 = pts(neighbor_nodes(i),:) - pt_this_node;
         v2 = pts(neighbor_nodes(j),:) - pt_this_node;
-        this_angle = acos( dot(v1,v2) / (norm(v1)*norm(v2)) ); 
+        this_angle = acos( dot(v1,v2) / (norm(v1)*norm(v2)) );
         angle_data(angle_idx,:) = [neighbor_nodes(i), neighbor_nodes(j), this_angle, norm(v1)+norm(v2)];
         angle_idx = angle_idx + 1;
     end
@@ -60,11 +72,56 @@ plot( pts(final_vertex_list(3),1), pts(final_vertex_list(3),2), 'bo', 'MarkerSiz
 
 % now do every other step....
 while( final_vertex_list(end) ~= final_vertex_list(1))
+    final_vertex_list
+    % get inward unit vector for previous segment
     this_node_idx = final_vertex_list(end);
+    pt_0 = pts(final_vertex_list(end-1),:);
+    pt_1 = pts(final_vertex_list(end),:);
+    pt_10 = pt_1 - pt_0;
+    prev_seg_inward_uv = [-pt_10(2) pt_10(1)];
+    prev_seg_inward_uv = prev_seg_inward_uv/norm(prev_seg_inward_uv);
+    plot([pt_0(1) pt_1(1)],[pt_0(2) pt_1(2)],'g-','LineWidth',2);
+    plot(pt_1(1)+[0 prev_seg_inward_uv(1)], pt_1(2)+[0 prev_seg_inward_uv(2)],'m-','LineWidth',2);
+    
+    % examine each of the connected nodes, and jump to the one that makes the
+    % largest angle
     neighbor_nodes = getNeighborNodes(proj_tri,this_node_idx);
+    angle_data = [];
+    for i = 1:length(neighbor_nodes)
+        if( neighbor_nodes(i) ~= final_vertex_list(end-1))
+            v1 = -pt_10;
+            v2 = pts(neighbor_nodes(i),:)-pt_1;
+            theta = acos( dot(v1,v2) / (norm(v1)*norm(v2)) );
+            if( dot(v2,prev_seg_inward_uv) > 0 )
+                this_angle = theta;
+            else
+                this_angle = 2*pi-theta;
+            end
+            angle_data(end+1,:) = [neighbor_nodes(i) this_angle norm(v2)];
+        end
+    end
+    angle_data((angle_data(:,2) == 2*pi) ,:) = [];
     
-    
+    angle_data = sortrows(angle_data,[2 3],'descend');
+    final_vertex_list(end+1) = angle_data(1,1);
 end
+
+% construct shadow polygon
+pts_poly = pts(final_vertex_list,:);
+proj_poly = polyshape( pts_poly(:,1), pts_poly(:,2),'Simplify',false);
+poly_area = area(proj_poly);
+[poly_cx poly_cy] = centroid(proj_poly);
+fprintf('Polygon area: %6.3f\n',poly_area);
+fprintf('Polygon centroid: (%6.3f,%6.3f)\n',poly_cx,poly_cy);
+figure; 
+subplot(1,2,1); 
+hold on; grid on; axis equal; 
+plot(pts_poly(:,1),pts_poly(:,2))
+subplot(1,2,2);
+hold on; grid on; axis equal;
+plot(poly);
+plot(poly_cx,poly_cy,'.','MarkerSize',30);
+
 
 % end
 
@@ -81,4 +138,6 @@ for i = 1:length(attached_tri_idx) % look at each triangle that uses this node
         end
     end
 end
+neighbor_nodes = unique(neighbor_nodes);
 end
+
