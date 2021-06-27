e1ef4464873cb871f09c8a214900da4ce928fad2
Mike Kokko <32647624+treelinemike@users.noreply.github.com>
2021-06-26 21:42:17 -0400

    normalize quaternion in quatrotate!

--- quat2angax.m
+++ quat2angax.m
@@ -1,4 +1,15 @@
 function angax = quat2angax(q)
+
+% make sure q_in is the correct length and shape
+if (numel(q_in) == 4)
+    q_in = reshape(q_in,4,1);
+else
+    error('Quaternion must have 4 elements.');
+end
+
+% normalize quaternion (it should be very close to a unit quaternion anyway...)
+q_in = q_in/norm(q_in);
+
 angax = zeros(4,1);
 halftheta = acos(q(1));
 if(halftheta < 10*eps)
--- quat2tang.m
+++ quat2tang.m
@@ -6,6 +6,10 @@ function tang_comp = quat2tang(q)
     if(size(q,1) ~= 4)
         error('Quaternion input must have 4 rows!');
     end
+    
+    % normalize quaternion (it should be very close to a unit quaternion anyway...)
+    q_in = q_in/norm(q_in);
+    
     theta = 2*acos(q(1,:));
     tang_comp = (theta./sin(theta/2)).*q(2:4,:);
 end
